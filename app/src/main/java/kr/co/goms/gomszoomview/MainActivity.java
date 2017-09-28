b04bd86f258a2340ed6a952964860294f9f8c33b
omaro7 <omaro7@hanmail.net>
2017-09-28 17:56:54 +0900

    commit test

--- app/src/main/java/kr/co/goms/gomszoomview/MainActivity.java
+++ app/src/main/java/kr/co/goms/gomszoomview/MainActivity.java
@@ -3,11 +3,15 @@ package kr.co.goms.gomszoomview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.co.goms.gomszoomview.util.GomsLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GomsLog.d("aa", "aa");
     }
 }
